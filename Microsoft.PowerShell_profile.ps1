9356829c67a65207f50f65f70e1d100c33d9c0d4
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2023-06-29 11:52:43 +0200

    add:pwsh profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+# https://github.com/JanDeDobbeleer/oh-my-posh
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\ys.omp.json" | Invoke-Expression
+# https://github.com/devblackops/Terminal-Icons
+Import-Module -Name Terminal-Icons
+
+function Sudo (
+	[Parameter(Mandatory)]
+	[string]$Program,
+	[Parameter()]
+	[string[]]$ArgumentList
+) {
+	if($ArgumentList) {
+		Start-Process $Program -Verb RunAs -ArgumentList $ArgumentList
+
+		return
+	}
+	Start-Process $Program -Verb RunAs
+}
